cd3b1f436875b5a531dbde4e9c61ec7623e04cb4
Jerome Bakker <jeabakker@coldtrick.com>
2017-07-14 15:15:32 +0200

    fixed: deprecated function usage

--- views/default/forms/filter_search.php
+++ views/default/forms/filter_search.php
@@ -2,20 +2,23 @@
 
 $page_owner_guid = elgg_get_page_owner_guid();
 if (!empty($page_owner_guid)) {
-	echo elgg_view_input('hidden', [
+	echo elgg_view_field([
+		'#type' => 'hidden',
 		'name' => '_po',
 		'value' => $page_owner_guid,
 	]);
 }
 
 if (elgg_get_page_owner_entity() instanceof ElggGroup) {
-	echo elgg_view_input('hidden', [
+	echo elgg_view_field([
+		'#type' => 'hidden',
 		'name' => '_cg',
 		'value' => $page_owner_guid,
 	]);
 }
 
-echo elgg_view_input('text', [
+echo elgg_view_field([
+	'#type' => 'text',
 	'name' => 'q',
 	'placeholder' => elgg_echo('search'),
 ]);
