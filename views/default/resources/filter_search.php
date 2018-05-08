8c54c168c2f9dcb14282ce391e6af892d333230a
Jerome Bakker <jeabakker@coldtrick.com>
2018-05-08 10:08:23 +0200

    added: support for pagination of filter search results

--- views/default/resources/filter_search.php
+++ views/default/resources/filter_search.php
@@ -3,6 +3,8 @@
 $search_params = elgg_extract('search_params', $vars, []);
 $search_params['search_type'] = 'entities';
 $search_params['query'] = get_input('q');
+$search_params['offset'] = max(0, (int) get_input('offset'));
+$search_params['limit'] = max(0, (int) get_input('limit', (int) elgg_get_config('default_limit')));
 
 $page_owner_guid = (int) get_input('_po');
 if (!empty($page_owner_guid)) {
@@ -25,6 +27,9 @@ if (empty($search_results)) {
 $content = elgg_view_entity_list($search_results['entities'], [
 	'full_view' => false,
 	'no_results' => elgg_echo('notfound'),
+	'count' => elgg_extract('count', $search_results, 0),
+	'offset' => elgg_extract('offset', $search_params),
+	'limit' => elgg_extract('limit', $search_params),
 ]);
 
 $title = elgg_echo("item:$type:$subtype");
